8937278230f6bc1bb85fb190e3b828751f71ea1b
Anh Tuan <tuanna.hsp@gmail.com>
2025-04-04 16:06:00 +0900

    Update config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,9 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
 
-  generate: {
-    routes: ["/"],
-  },
+  // generate: {
+  //   routes: ["/"],
+  // },
 
   ssr: false,
 
